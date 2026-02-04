e59c8a58f414502de779a24954c9c84f2dd095c0
nitai.gildor <nitai.gildor@mail.huji.ac.il>
2026-02-04 18:21:32 +0200

    improved initial structure

--- fluidics_module.py
+++ fluidics_module.py
@@ -7,20 +7,35 @@ class FluidicsDriver:
         Control Unit constructor. Creates movement system, fluidics system and CV objects.
         :return: MovementDriver object
         """
-        try:
-            self.fluidic_module = fluidics_module()
-            self.movement_module = movement_driver.MovementDriver()
-            self.cv_module = cv_module()
-        except:
-            print("Subsystem initiation failed")
-            exit(1)
+        # Probably need to read from json here (comport etc):
+
+        self.ready = False
 
     def __del__(self):
         """
         Fluidics system destructor. Stops system action and then closes port
         :return: VOID
         """
-        # Close movement serial
-        self.movement_module.__del__()
-        self.cv_module.__del__()
-        self.fluidics_module.__del__()
+
+    def output(self, amount):
+        """
+        Output the given amount of fluid. Requires the fill tube function to be used first
+        :param amount: amount of food to output (im micro-liters)
+        :return: boolean true for success, false otherwise
+        """
+        if not self.ready:
+            print("Tube was not initialized yet")
+            return False
+
+    def flush(self):
+        """
+        Flushes the tube (used for cleaning). Will either use keypress to stop or a predefined amount
+        :return: true when finished, false otherwise
+        """
+
+    def fill_tube(self):
+        """
+        Fills the tube before feeding (above empty container). Will use a predefined amount
+        :return: true when finished, false otherwise
+        """
+        self.ready = True
